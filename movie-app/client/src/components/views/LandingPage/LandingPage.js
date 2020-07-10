53b589d8d60533af515efd1b1e9400b8470d598f
Pankhuri <pankhuritrikha@gmail.com>
2020-07-10 20:33:54 +0530

    Fetching of movie-db api in the LangingPage

--- movie-app/client/src/components/views/LandingPage/LandingPage.js
+++ movie-app/client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { FaCode } from "react-icons/fa";
+import {API_URL, API_KEY} from '../../Config';
+//import { response } from 'express';
 
 function LandingPage() {
+
+    useEffect(() => {
+        fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+            })
+    }, [])
+
     return (
         <>
         <div className="app">
