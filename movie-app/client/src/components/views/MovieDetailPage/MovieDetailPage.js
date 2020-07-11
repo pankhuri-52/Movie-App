2995c27c8a44ceeff9c61e24a0a46cc7f8834891
Pankhuri <pankhuritrikha@gmail.com>
2020-07-11 13:35:06 +0530

    Made an entire template for a specific movie page

--- movie-app/client/src/components/views/MovieDetailPage/MovieDetailPage.js
+++ movie-app/client/src/components/views/MovieDetailPage/MovieDetailPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import {API_URL, API_KEY} from '../../Config'; 
+import {API_URL, API_KEY, IMAGE_URL} from '../../Config'; 
+import MainImage from '../LandingPage/Sections/MainImage';
+import { Descriptions, Button } from 'antd';
 
 function MovieDetailPage(props) {
 
@@ -20,6 +22,38 @@ function MovieDetailPage(props) {
 
     return (
         <div>
+            {/* Main Image */}
+            {
+                Movie &&
+                <MainImage image={`${IMAGE_URL}/w1280${Movie.backdrop_path && Movie.backdrop_path}`} 
+                title={Movie.original_title} 
+                text={Movie.overview} />
+            }
+
+            {/* Body */}
+            <div style={{width : '85%', margin : '1rem auto'}}>
+                <div style={{display:'flex' , justifyContent:'flex-end'}}>
+                    <Button>Add to Favorite</Button>
+                </div>
+
+                {/* MovieInfo Table */}
+                <Descriptions title="Movie Info" bordered>
+                    <Descriptions.Item label="Title">{Movie.original_title}</Descriptions.Item>
+                    <Descriptions.Item label="release_date">{Movie.release_date}</Descriptions.Item>
+                    <Descriptions.Item label="revenue">{Movie.revenue}</Descriptions.Item>
+                    <Descriptions.Item label="runtime">{Movie.runtime}</Descriptions.Item>
+                    <Descriptions.Item label="vote_average" span={2}>{Movie.vote_average}</Descriptions.Item>
+                    <Descriptions.Item label="vote_count">{Movie.vote_count}</Descriptions.Item>
+                    <Descriptions.Item label="status">{Movie.status}</Descriptions.Item>
+                    <Descriptions.Item label="popularity">{Movie.popularity}</Descriptions.Item>
+                </Descriptions>
+
+                {/* Toggle Button */}
+                <div style={{display:'flex' , justifyContent:'center', padding: '10px'}}>
+                    <Button>Toggle Actor View</Button>
+                </div>
+
+            </div>
 
         </div>
     )
