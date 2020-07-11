49e40ccd82134e6807cc75a43810a525ca1ff34f
Pankhuri <pankhuritrikha@gmail.com>
2020-07-11 12:40:52 +0530

    Fetched detail info that belongs to a specific movie

--- movie-app/client/src/components/views/MovieDetailPage/MovieDetailPage.js
+++ movie-app/client/src/components/views/MovieDetailPage/MovieDetailPage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {API_URL, API_KEY} from '../../Config'; 
 
-function MovieDetailPage() {
+function MovieDetailPage(props) {
+
+    useEffect(() => {
+
+        const movieId = props.match.params.movieId;
+
+        fetch(`${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+            })
+
+    },[])
+
     return (
         <div>
-            
+
         </div>
     )
 }
